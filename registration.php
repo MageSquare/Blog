03014d98bd99c88108934b06ea04e9c3fdfcd4c1
MageSquare <92527816+MageSquare@users.noreply.github.com>
2021-10-14 18:49:56 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author MageSquare Team
+ * @copyright Copyright (c) 2021 MageSquare 
+ * @package MageSquare_Blog
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MageSquare_Blog',
+    __DIR__
+);
